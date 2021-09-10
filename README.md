e294f049bf32146bb3828e0d636998e18407e37c
libin <1064048947@qq.com>
2021-09-10 15:47:36 +0800

    lazyload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # IntersectionObserver
 Intersection Observer 能做什么?
+## 参考
+https://developer.mozilla.org/zh-CN/docs/Web/API/Intersection_Observer_API
+https://juejin.cn/post/6995758112779010084
+
+## lazyload
+Intersection API 观察具体的 img，检测在用户的可视范围内即触发 load img
---
+++ lazyload/index.js
@@ -0,0 +1,42 @@
+function insert(el) {
+  function loadImage() {
+    el.addEventListener('load', () => {
+      // 加载完成添加class可以实现淡入动画
+      el.classList.add('loaded')
+    });
+
+    // 加载 data-original 的图片地址
+    el.src = el.dataset.original;
+  }
+
+  function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        // 绑定元素进入视口后触发加载图片
+        loadImage();
+        // 并停止观察可见性变化, 防止再次加载图像。
+        observer.unobserve(el);
+      }
+    });
+  }
+
+  function createObserver() {
+    const options = {
+      root: null,
+      threshold: '0'
+    };
+
+    const observer = new IntersectionObserver(handleIntersect, options);
+
+    // 订阅观察当前绑定图片元素
+    observer.observe(el);
+  }
+
+  createObserver();
+}
+
+for (let i = 1; i < 13; i++) {
+  insert(document.getElementById(`test${i}`))
+}
